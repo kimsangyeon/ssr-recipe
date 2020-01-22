7740eef03e97a9fb9d13c952b23a2a9682efe4d4
sangyeon <dho113@naver.com>
2020-01-22 17:29:28 +0900

    createPage static files

--- src/index.server.js
+++ src/index.server.js
@@ -3,6 +3,40 @@ import ReactDOMServer from 'react-dom/server';
 import express from 'express';
 import {StaticRouter} from 'react-router-dom'; // props location에 따라 라우팅 해주는 라우터
 import App from './App';
+import path from 'path';
+import fs from 'fs';
+
+const manifest = JSON.parse(
+  fs.readFileSync(path.resolve('./build/asset-manifest.json'), 'utf8')
+);
+
+const chunks = Object.keys(manifest.files)
+  .filter(key => /chunk\.js$/.exec(key)) // chunk.js로 끝나는 키를 찾아서
+  .map(key => `<script src="${manifest['files'][key]}"></script>`) // 스크립트 태그로 변환
+  .join('');
+
+function createPage(root) {
+  return `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <link rel="shortcut icon" href="/favicon.ico" />
+    <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no" />
+    <meta name="theme-color" content="#000000" />
+    <title>React App</title>
+    <link href="${manifest['files']['main.css']}" rel="stylesheet" />
+  </head>
+  <body>
+    <noscript>You need to enable Javawscript to run this app.</noscript>
+    <div id="root">
+      ${root}
+    </div>
+    <script src="${manifest['files']['runtime-main.js']}"></script>
+    ${chunks}
+    <script src="${manifest['files']['main.js']}"></script>
+  </body>
+  </html>`;
+}
 
 const app = express();
 
@@ -11,15 +45,22 @@ const serverRender = (req, res, next) => {
   // 이 함수는 404가 떠야 하는 상황에 404를 띄우지 않고 서버 사이드 랜더링을 해줍니다.
   
   const context = {};
+  console.log(manifest);
   const jsx = (
     <StaticRouter location={req.url} context={context}>
       <App />
     </StaticRouter>
   );
   const root = ReactDOMServer.renderToString(jsx);
-  res.send(root);
+  console.log(createPage(root));
+  res.send(createPage(root));
 };
 
+const serve = express.static(path.resolve('./build'), {
+  index: false // "/" 경로에서 index.html을 보여 주지 않도록 설정
+});
+
+app.use(serve);
 app.use(serverRender);
 
 app.listen(5000, () => {
